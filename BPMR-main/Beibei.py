aeda4f19d5fc48f14b73df4844a32e4c3492ddb5
rkitleex <159437651+rkitleex@users.noreply.github.com>
2024-02-08 22:25:58 +1100

    Add files via upload

---
+++ BPMR-main/Beibei.py
@@ -0,0 +1,22 @@
+import random
+import torch
+import numpy as np
+from zctr.Step1CreateIIndex import Step1CreateIIndex
+from zctr.Step2CreateFeatures import Step2CreateFeatures
+from zctr.StepTNBZScore import StepTNBZScore
+
+
+SEED = 1
+def seed_everything(seed=SEED):
+    random.seed(seed)
+    np.random.seed(seed)
+
+
+if __name__ == "__main__":
+
+    seed_everything(SEED)
+
+    dataset = "beibei"
+    Step1CreateIIndex.execute(dataset)
+    Step2CreateFeatures.execute(dataset)
+    StepTNBZScore.execute(dataset)
---
+++ BPMR-main/README.md
@@ -0,0 +1,12 @@
+# BPMR
+## Example to run the codes
+### Beibei
+``` bash
+python Beibei.py;
+```
+### Taobao
+``` bash
+python Taobao.py.
+```
+
+### In addition, in order to better analyze our model, in this version we do not use GPUs to accelerate and store some intermediate experimental data, which are not needed at all in the real environment.
